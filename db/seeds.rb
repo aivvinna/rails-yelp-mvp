439cf287c81d6e7be51d7cbbdf1373481f09f5ac
Hiroki Furuichi <aivvinna@gmail.com>
2018-08-08 15:44:03 +0900

    yelp rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = Restaurant.create([{ name: 'french restaurant', address: 'france', phone_number: 11111, category: 'french' },
+ { name: 'italian restaurant', address: 'italy', phone_number: 22222, category: 'italian' },
+ { name: 'japanese restaurant', address: 'japan', phone_number: 33333, category: 'japanese'},
+ { name: 'chinese restaurant', address: 'china', phone_number: 44444, category: 'chinese'},
+ { name: 'belgian restaurant', address: 'belgium', phone_number: 55555, category: 'belgian'}])
